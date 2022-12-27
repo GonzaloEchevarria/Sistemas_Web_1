bc9d75779361c676018dfbc7d7c3e5d6302e51c8
GonzaloEchevarria <g.echevarria5@usp.ceu.es>
2022-12-27 20:36:09 +0100

    registro hecho

--- routes/registro.js
+++ routes/registro.js
@@ -1,9 +1,47 @@
 const express = require('express');
+const db = require('../database');
 const router = express.Router();
 const users = require('../users');
 
+router.get('/', function(req, res, next) {
+  res.render('registro', { title: 'Register', user: req.session.user}); /* registro y titulo */
+});
 
-/* GET home page. */
+router.post('/', function(req, res, next){
+    let user = req.body.user;
+    let password = req.body.pass;
+    let rep_password = req.body.pass2;
+    let rol = req.body.rol;
+    let longitud_pass = password.length;
+    let longitud_user = user.length;
+
+    if(users[user]){
+        req.session.error = "This Username is in use!";
+        res.redirect("/registro");
+    }
+    
+    else if (1 <= longitud_user && 8 <= longitud_pass && password == rep_password){ //&& users.registro_completo(user, password,rep_password)==true){
+        console.log("ok");
+        db.register(user,password,rol, function(){
+            req.session.user = users[user]; /* para iniciar sesión automáticamente tras el registro */
+            //req.session.message = "Register CORRECT!";
+            res.redirect("/restricted");
+        });
+    }
+
+    else{
+        req.session.error = "ERROR, Asegurese de que las contraseñas coinciden y que su longitud es igual o mayor a 8";
+        res.redirect("/registro"); /* para que salga el mensaje es importante tener una res */
+    }
+});
+
+module.exports = router;
+
+
+
+
+
+/* GET home page. 
 router.get('/', function(req, res, next) {
     res.render('registro', { title: 'Registro', user: req.session.user});
   });
@@ -23,7 +61,7 @@ router.post('/', function(req, res, next){
     }   
 });
 
-
+*/
 
 
   module.exports = router;
