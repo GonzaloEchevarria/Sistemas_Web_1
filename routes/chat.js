fac6b7109c291d6e92565462147ebb30c381183c
GonzaloEchevarria <g.echevarria5@usp.ceu.es>
2023-01-02 19:10:40 +0100

    arreglo chat

--- routes/chat.js
+++ routes/chat.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 
 router.get('/', function(req, res, next) {
-  res.render('chat', { title: 'Chat', user: req.session.user});
+  res.render('chat', { title: 'Chat', user: req.session.user, rol:req.session.rol});
 });
 
 module.exports = router;
