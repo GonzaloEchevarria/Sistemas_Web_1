73c13b4139620135c3d9d0e52caaa0992d10872e
Diego Herrero <d.herrero8@usp.ceu.es>
2023-01-10 21:39:53 +0100

    login fix

--- routes/login.js
+++ routes/login.js
@@ -12,21 +12,32 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next){
     let user = req.body.user;   
-    db.login(req.body.user,req.body.pass,function(err, result){
-        if(result){
-            req.session.user = user;
-            db.obtenerRol(user,function(rol){
-                req.session.rol=rol;
-                res.redirect("/tablon");
-            })
-            
-        } else {
+    db.registroExiste(user,function(validacion){
+        console.log("VAL:"+validacion);
+        if(validacion===false){
+            console.log("es falso");
             req.session.rol=undefined;
             req.session.error="Incorrect user or password";
             res.redirect("/");
         }
+        else{
+            db.login(req.body.user,req.body.pass,function(err, result){
+                if(result){
+                    req.session.user = user;
+                    db.obtenerRol(user,function(rol){
+                        req.session.rol=rol;
+                        res.redirect("/tablon");
+                    })
+                    
+                } else {
+                    req.session.rol=undefined;
+                    req.session.error="Incorrect user or password";
+                    res.redirect("/");
+                }
+            });
+        }
     });
-    
+       
    
 });
 
